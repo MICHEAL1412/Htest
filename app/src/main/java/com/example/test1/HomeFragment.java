d3ff242b2dd314aebd4d353dcdb02025643f84fe
Micheal <michealnkim-wm19@student.tarc.edu.my>
2020-08-25 17:53:35 +0800

    commit by ng

--- app/src/main/java/com/example/test1/HomeFragment.java
+++ app/src/main/java/com/example/test1/HomeFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +15,9 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+
         return inflater.inflate(R.layout.fragment_home,container,false);
     }
 }
